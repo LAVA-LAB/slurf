aefc18a1a0eb6ddeebe8dc5fce6610a6b406c55d
Matthias Volk <m.volk@utwente.nl>
2021-12-09 18:15:49 +0100

    Created interface for CTMCReliabilityModelSampler

--- slurf/model_sampler_interface.py
+++ slurf/model_sampler_interface.py
@@ -1,10 +1,18 @@
 import stormpy as sp
+import stormpy.pars
 import math
 
 class ModelSamplerInterface:
     """
     Describes the interface for sampling parametric models.
     """
+
+    def __init__(self):
+        self._model_file = None
+        self._init_state = None
+        self._properties = None
+        self._parameters = None
+        self._inst_checker = None
 
     def load(self, model, properties):
         """
@@ -45,13 +53,8 @@ class ModelSamplerInterface:
 
 class CtmcReliabilityModelSamplerInterface(ModelSamplerInterface):
     """
-    This simple interface builds a parametric CTMC and then uses an instantation checker to check the model.
+    This simple interface builds a parametric CTMC and then uses an instantiation checker to check the model.
     """
-
-    def __init__(self):
-        self._model = None
-        self._properties = None
-        self._parameters = None
 
     def load(self, model, properties):
         """
@@ -65,19 +68,45 @@ class CtmcReliabilityModelSamplerInterface(ModelSamplerInterface):
         -------
 
         """
+        # Load prism program
         program = sp.parse_prism_program(model)
+        # Create properties
         event, time_bounds = properties[0], properties[1]
         property_strings = [f'P=? [ F<={float(t)} "{event}" ]' for t in time_bounds]
         self._properties = sp.parse_properties_for_prism_program(";".join(property_strings), program)
+        # Build (sparse) CTMC
         options = sp.BuilderOptions([p.raw_formula for p in self._properties])
         self._model = sp.build_sparse_parametric_model_with_options(program, options)
+        # Get (unique) initial state
+        assert len(self._model.initial_states) == 1
+        self._init_state = self._model.initial_states[0]
+        # Get parameters
         self._parameters = list(self._model.collect_all_parameters())
-        # TODO instantiation checker
+        # Create instantiation model checker
+        self._inst_checker = sp.pars.PCtmcInstantiationChecker(self._model)
+        # Return all parameters each with range (0 infinity)
         return { p: (0, math.inf) for p in self._parameters}
 
 
     def sample(self, id, sample_point, property_ids=None, store=False):
-        # TODO query instantiatino checker.
-        pass
+        # Set property ids (use all if none are given)
+        prop_ids = range(len(self._properties)) if property_ids is None else property_ids
+        # Create sample point
+        point = {p: sp.RationalRF(val) for p, val in sample_point.items()}
+        # Sample point must be graph preserving
+        self._inst_checker.set_graph_preserving(True)
 
+        env = sp.Environment()
+        results = {}
+        # Analyse each property individually (Storm does not allow multiple properties for the InstantiationModelChecker
+        for prop_id in prop_ids:
+            # Specify formula
+            formula = self._properties[prop_id].raw_formula
+            self._inst_checker.specify_formula(sp.ParametricCheckTask(formula, True)) # Only initial states
+            # Check CTMC
+            result = self._inst_checker.check(env, point).at(self._init_state)
+            # Add result
+            results[prop_id] = result
+
+        return results
 
--- test/test_model_sampler_interface.py
+++ test/test_model_sampler_interface.py
@@ -1,13 +1,19 @@
 from slurf.model_sampler_interface import CtmcReliabilityModelSamplerInterface
 from . import util as testutils
 
+import math
+
 
 class TestModelSampler:
     def test_CTMC(self):
         sampler = CtmcReliabilityModelSamplerInterface()
         parameters_with_bounds = sampler.load(testutils.tiny_pctmc, ("full", [1,5,10]))
         parameter_objects_by_name = {p.name : p for p in parameters_with_bounds.keys()}
-        sampler.sample(1, {parameter_objects_by_name["p"] : 0.3})
+        assert "p" in parameter_objects_by_name
+        result = sampler.sample(1, {parameter_objects_by_name["p"] : 0.3})
+        assert math.isclose(result[0], 0.1734083474)
+        assert math.isclose(result[1], 0.9427719189)
+        assert math.isclose(result[2], 0.9987049333)
 
 
 
