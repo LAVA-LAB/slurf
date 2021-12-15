484c69ad6d5eba843d52d235215099089d649fa7
Thom Badings <thom.badings@ru.nl>
2021-12-15 16:28:52 +0100

    removed exponential test; improved scenario problem

--- runfile.py
+++ runfile.py
@@ -1,13 +1,13 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 import seaborn as sns
 from slurf.scenario_problem import scenarioProblem
 
 from slurf.model_sampler_interface import \
-    CtmcReliabilityModelSamplerInterface, \
-    DftReliabilityModelSamplerInterface
+    CtmcReliabilityModelSamplerInterface
 
 
 def _path(folder, file):
@@ -22,90 +22,146 @@ def _path(folder, file):
     return os.path.join(testfile_dir, folder, file)
 
 
+def sample_solutions(Nsamples, Tlist, model):
+    """
+
+    Parameters
+    ----------
+    Nsamples Number of samples
+    Tlist List of time points to evaluate at
+    model File name of the model to load
+
+    Returns 2D Numpy array with every row being a sample
+    -------
+
+    """
+
+    # Load model
+    sampler = CtmcReliabilityModelSamplerInterface()
+    parameters_with_bounds = sampler.load(model, ("done", Tlist))
+    param_obj_by_name = {p.name: p for p in parameters_with_bounds.keys()}
+
+    samples = np.zeros((Nsamples, len(Tlist)))
+    parameters = np.random.uniform(low=[0.05, 0.05], high=[0.3, 0.3],
+                                   size=(Nsamples, 2))
+
+    for n in range(Nsamples):
+
+        print('Start for sample', str(n) + '..')
+
+        # Set parameters
+        ki, kr = parameters[n]
+
+        # Set parameter values in dictionary
+        param_values = {
+            param_obj_by_name["ki"]: ki,
+            param_obj_by_name["kr"]: kr
+            }
+
+        # Compute results
+        samples[n] = list(sampler.sample(1, param_values).values())
+
+    return samples
+
+
+def compute_slurf(Tlist, samples, Nsamples, rho_list):
+    """
+
+    Parameters
+    ----------
+    Tlist List of time points to evaluate at
+    samples 2D Numpy array of samples
+    Nsamples Number of samples
+    rho_list List of the values of rho for which the scenario program is solved
+    -------
+
+    """
+
+    # Create plot
+    fig, ax = plt.subplots()
+    plt.plot(Tlist, samples.T, color='k', lw=0.3, ls='dotted', alpha=0.3)
+
+    # Set colors and markers
+    colors = sns.color_palette()
+    markers = ['o', '*', 'x', '.', '+']
+
+    x_low = {}
+    x_upp = {}
+    violation_prob = np.zeros(len(rho_list))
+
+    problem = scenarioProblem(samples)
+
+    for i, rho in enumerate(rho_list):
+
+        x_mean, x_width, c_star, x_star = problem.rectangular(rho)
+
+        print("\nThe optimal value is", x_star)
+        print('Complexity of penalty-based program:', c_star)
+
+        from compute_eta import etaLow, betaLow
+
+        beta_desired = 0.99
+
+        violation_prob[i] = 1 - etaLow(Nsamples, c_star, beta_desired)
+
+        print('Upper bound on violation probability:', violation_prob[i])
+
+        confidence_prob = betaLow(Nsamples, c_star, 1-violation_prob[i])
+
+        print('Error with desired beta:',
+              np.abs(beta_desired - confidence_prob))
+
+        # Plot confidence regions
+        labelStr = r'$\rho$: '+str(rho) + \
+            r'; $s^*$: '+str(c_star) + \
+            r'; $\epsilon$: ' + str(np.round(violation_prob[i], 2))
+
+        x_low[i] = x_mean - x_width
+        x_upp[i] = x_mean + x_width
+
+        plt.plot(Tlist, x_low[i], lw=2, marker=markers[i],
+                 color=colors[i], label=labelStr)
+        plt.plot(Tlist, x_upp[i], lw=2, marker=markers[i],
+                 color=colors[i])
+
+    plt.xlabel('Time')
+    plt.ylabel('Probability of zero infected')
+
+    ax.set_title("Probability of zero infected population over time (N={} samples)".format(Nsamples))
+
+    plt.legend()
+    plt.show()
+
+    return x_low, x_upp
+
+
+def printTime():
+
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+
+    return current_time
+
+# %%
+
+
+print("Script started at:", printTime())
+
 # Generate samples
-Tend = 30
-Tlist = np.arange(0, Tend+1, 5)
+Tlist = np.arange(10, 100+1, 15)
 
-Nsamples = 100
+Nsamples = 400
 
-MODEL = _path("", "sir.sm")
-
-# Load model
-sampler = CtmcReliabilityModelSamplerInterface()
-parameters_with_bounds = sampler.load(MODEL, ("done", Tlist))
-parameter_objects_by_name = {p.name: p for p in parameters_with_bounds.keys()}
-
-assert "ki" in parameter_objects_by_name and "kr" in parameter_objects_by_name
-
-samples = np.zeros((Nsamples, len(Tlist)))
-
-fig, ax = plt.subplots()
-
-for n in range(Nsamples):
-
-    print('Start for sample', str(n) + '..')
-
-    # Sample parameters
-    ki, kr = np.random.uniform(low=[0.05, 0.05], high=[0.3, 0.3])
-
-    # Set parameter values in dictionary
-    param_values = {
-        parameter_objects_by_name["ki"]: ki,
-        parameter_objects_by_name["kr"]: kr
-        }
-
-    # Compute results
-    samples[n] = list(sampler.sample(1, param_values).values())
-
-    # if Nsamples < 1000 or n % int(Nsamples/100) == 0:
-    plt.plot(Tlist, samples[n, :], color='k', lw=0.3, ls='dotted', alpha=0.3)
-
-# Compute bounds on violation probability with risk and complexity theory
-
-# Set colors and markers
-colors = sns.color_palette()
-markers = ['o', '*', 'x', '.', '+']
+# Generate given number of solutions to the parametric model
+model = _path("", "sir.sm")
+samples = sample_solutions(Nsamples, Tlist, model)
 
 # Values of rho (cost of regret) at which to solve the scenario program
-rho_list = [2, 1, 0.5, 0.3, 0.1]
+rho_list = [2, 0.5, 0.1, 0.05, 0.01]
 
-x_low = {}
-x_upp = {}
-violation_prob = np.zeros(len(rho_list))
+# %%
 
-problem = scenarioProblem(samples)
+# Compute SLURF and plot
+x_low, x_upp = compute_slurf(Tlist, samples, Nsamples, rho_list)
 
-for i, rho in enumerate(rho_list):
-
-    x_mean, x_width, c_star, x_star = problem.rectangular(rho)
-
-    print("\nThe optimal value is", x_star)
-    print('Complexity of penalty-based program:', c_star)
-
-    from compute_eta import etaLow, betaLow
-
-    beta_desired = 0.99
-
-    violation_prob[i] = 1 - etaLow(Nsamples, c_star, beta_desired)
-
-    print('Upper bound on violation probability:', violation_prob[i])
-
-    confidence_prob = betaLow(Nsamples, c_star, 1-violation_prob[i])
-
-    print('Error with desired beta:', np.abs(beta_desired - confidence_prob))
-
-    # Plot confidence regions
-    labelStr = r'$\rho$: '+str(rho) + \
-        '; $s^*$: '+str(c_star) + \
-        '; $\epsilon$: ' + str(np.round(violation_prob[i], 2))
-
-    x_low[i] = np.hstack((0, x_mean - x_width))
-    x_upp[i] = np.hstack((0, x_mean + x_width))
-
-    plt.plot(Tlist, x_low[i], lw=2, marker=markers[i],
-             color=colors[i], label=labelStr)
-    plt.plot(Tlist, x_upp[i], lw=2, marker=markers[i],
-             color=colors[i])
-
-plt.legend()
-plt.show()
+print("Script done at:", printTime())
--- slurf/scenario_problem.py
+++ slurf/scenario_problem.py
@@ -1,76 +1,116 @@
 import numpy as np
 import cvxpy as cp
+import math
 
 class scenarioProblem:
     """
     Functions related to the scenario optimization part.
     """
-    
+
     def __init__(self, samples):
         """
-    
+
         Parameters
         ----------
         samples n x m array, with each row an m-dimensional sample
-    
+
         """
-        
+
         self.samples = samples
         self.Nsamples, self.dim = np.shape(self.samples)
-    
+
     def rectangular(self, costOfRegret = 1):
         """
-    
+
         Parameters
         ----------
         costOfRegret cost of violation parameter (rho in the paper)
-        
+
         Returns
         -------
         Mean and width of optimal solution, complexity, and optimal cost.
-        
+
         """
-    	
+
+        obj, x_mean, x_width, prob, constraints_low, constraints_upp = \
+            self._solve(self.samples, costOfRegret)
+
+        x_star = prob.value
+
+        # Determine complexity of the solution
+        support_set = np.ones(self.Nsamples)
+
+        # For every non-relaxed sample, check its dual values
+        for i in range(self.Nsamples):
+            # # Otherwise, check its dual values
+
+            LB_dual = constraints_low[i].dual_value
+            UB_dual = constraints_upp[i].dual_value
+
+            # If all dual values are zero, sample is not associated with any
+            # active constraint
+            active_count = sum(~np.isclose(LB_dual, 0)) + sum(~np.isclose(UB_dual, 0))
+            support_set[i] = active_count
+
+        # Remove every sample which is definitely not of support
+        samples_bar = self.samples[support_set > 0, :]
+
+        # Iteratively remove other samples
+        done = False
+        while not done:
+
+            something_removed = False
+
+            for n in range(len(samples_bar)):
+                mask = np.zeros(len(samples_bar), dtype=bool)
+                mask[n] = 1
+
+                _, y_mean, y_width, _, _, _ = self._solve(samples_bar[mask], costOfRegret)
+
+                # If objective is still the same, remove this sample
+                if all(np.isclose(x_mean.value, y_mean.value)) and \
+                   all(np.isclose(x_width.value, y_width.value)):
+                    something_removed = True
+
+                    # Remove current sample from set
+                    samples_bar = samples_bar[mask]
+                    break
+
+            if not something_removed:
+                done = True
+
+        complexity = len(samples_bar)
+        print('Complexity ')
+
+        return x_mean.value, x_width.value, complexity, x_star
+
+    def _solve(self, samples, costOfRegret):
+
+        Nsamples, dim = np.shape(samples)
+
         # Define convex optimization program
-        x_mean  = cp.Variable(self.dim-1, nonneg=True)
-        x_width = cp.Variable(self.dim-1, nonneg=True)
-        
+        x_mean = cp.Variable(dim, nonneg=True)
+        x_width = cp.Variable(dim, nonneg=True)
+
         # Define regret/slack variables
-        xi      = cp.Variable(self.Nsamples, nonneg=True)
-        
+        xi = cp.Variable((Nsamples, dim), nonneg=True)
+
         # Cost of violation
         rho = cp.Parameter()
         rho.value = costOfRegret
-        
+
         constraints_low = []
         constraints_upp = []
-        
+
         # Add constraints for each samples
-        for n in range(self.Nsamples):
-            
-            constraints_low += [self.samples[n, 1:] >= x_mean - x_width - xi[n]]
-            constraints_upp += [self.samples[n, 1:] <= x_mean + x_width + xi[n]]
-            
-        obj = cp.Minimize( sum(x_width) + rho * sum(xi) )
-            
+        for n in range(Nsamples):
+
+            constraints_low += [samples[n, :] >= x_mean - x_width - xi[n, :]]
+            constraints_upp += [samples[n, :] <= x_mean + x_width + xi[n, :]]
+
+        obj = cp.Minimize(sum(x_width) + rho * sum(xi @ np.ones(dim)))
+
         prob = cp.Problem(obj, constraints_low + constraints_upp)
-        prob.solve( solver='GUROBI' )
-        
-        x_star = prob.value
-        
-        # Determine complexity of the solution
-        complexity = 0
-        for n in range(self.Nsamples):
-            
-            # If duel value of a constraint is nonzero, that constraint is active
-            if any(np.abs(constraints_low[n].dual_value) > 1e-3):
-                complexity += 1
-                
-            if any(np.abs(constraints_upp[n].dual_value) > 1e-3):
-                complexity += 1
-        
-        return x_mean.value, x_width.value, complexity, x_star
-        
-        
-        
-       
+        prob.solve(solver='GUROBI')
+
+        return obj, x_mean, x_width, prob, constraints_low, constraints_upp
