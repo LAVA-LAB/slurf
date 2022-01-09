3239f83f3a979884ff0a36337b0fec23d4e16c3d
Thom Badings <thom.badings@ru.nl>
2022-01-09 20:04:49 +0100

    added run scripts

--- run_all_kanban_experiments.sh
+++ run_all_kanban_experiments.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-python3 runfile.py --model ctmc/kanban/kanban2.sm --N 50 --Nvalidate 1000 --no-bisim;
-python3 runfile.py --model ctmc/kanban/kanban2.sm --N 100 --Nvalidate 1000 --no-bisim;
-python3 runfile.py --model ctmc/kanban/kanban2.sm --N 200 --Nvalidate 1000 --no-bisim;
-python3 runfile.py --model ctmc/kanban/kanban2.sm --N 400 --Nvalidate 1000 --no-bisim;
+python3 runfile.py --model ctmc/kanban/kanban2.sm --N 50 --no-bisim;
+python3 runfile.py --model ctmc/kanban/kanban2.sm --N 100 --no-bisim;
+python3 runfile.py --model ctmc/kanban/kanban2.sm --N 200 --no-bisim;
+#python3 runfile.py --model ctmc/kanban/kanban2.sm --N 400 --no-bisim;
 ###
-python3 runfile.py --model ctmc/kanban/kanban3.sm --N 50 --Nvalidate 1000 --no-bisim;
-python3 runfile.py --model ctmc/kanban/kanban3.sm --N 100 --Nvalidate 1000 --no-bisim;
-python3 runfile.py --model ctmc/kanban/kanban3.sm --N 200 --Nvalidate 1000 --no-bisim;
-python3 runfile.py --model ctmc/kanban/kanban3.sm --N 400 --Nvalidate 1000 --no-bisim;
+python3 runfile.py --model ctmc/kanban/kanban3.sm --N 50 --no-bisim;
+python3 runfile.py --model ctmc/kanban/kanban3.sm --N 100 --no-bisim;
+python3 runfile.py --model ctmc/kanban/kanban3.sm --N 200 --no-bisim;
+#python3 runfile.py --model ctmc/kanban/kanban3.sm --N 400 --no-bisim;
 ###
-python3 runfile.py --model ctmc/kanban/kanban4.sm --N 50 --Nvalidate 1000 --no-bisim;
-python3 runfile.py --model ctmc/kanban/kanban4.sm --N 100 --Nvalidate 1000 --no-bisim;
-python3 runfile.py --model ctmc/kanban/kanban4.sm --N 200 --Nvalidate 1000 --no-bisim;
-python3 runfile.py --model ctmc/kanban/kanban4.sm --N 400 --Nvalidate 1000 --no-bisim;
+python3 runfile.py --model ctmc/kanban/kanban4.sm --N 50 --no-bisim;
+python3 runfile.py --model ctmc/kanban/kanban4.sm --N 100 --no-bisim;
+python3 runfile.py --model ctmc/kanban/kanban4.sm --N 200 --no-bisim;
+#python3 runfile.py --model ctmc/kanban/kanban4.sm --N 400 --no-bisim;
 ###
-python3 runfile.py --model ctmc/kanban/kanban5.sm --N 50 --Nvalidate 1000 --no-bisim;
-python3 runfile.py --model ctmc/kanban/kanban5.sm --N 100 --Nvalidate 1000 --no-bisim;
-python3 runfile.py --model ctmc/kanban/kanban5.sm --N 200 --Nvalidate 1000 --no-bisim;
-python3 runfile.py --model ctmc/kanban/kanban5.sm --N 400 --Nvalidate 1000 --no-bisim;
+python3 runfile.py --model ctmc/kanban/kanban5.sm --N 50 --no-bisim;
+python3 runfile.py --model ctmc/kanban/kanban5.sm --N 100 --no-bisim;
+python3 runfile.py --model ctmc/kanban/kanban5.sm --N 200 --no-bisim;
+#python3 runfile.py --model ctmc/kanban/kanban5.sm --N 400 --no-bisim;
---
+++ run_all_polling_experiments.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#python3 runfile.py --model ctmc/polling/polling3.sm --N 50;
+python3 runfile.py --model ctmc/kanban/polling3.sm --N 100;
+#python3 runfile.py --model ctmc/kanban/polling3.sm --N 200;
+#python3 runfile.py --model ctmc/kanban/polling3.sm --N 400;
+###
+#python3 runfile.py --model ctmc/kanban/polling6.sm --N 50;
+python3 runfile.py --model ctmc/kanban/polling6.sm --N 100;
+#python3 runfile.py --model ctmc/kanban/polling6.sm --N 200;
+#python3 runfile.py --model ctmc/kanban/polling6.sm --N 400;
+###
+#python3 runfile.py --model ctmc/kanban/polling9.sm --N 50;
+python3 runfile.py --model ctmc/kanban/polling9.sm --N 100;
+#python3 runfile.py --model ctmc/kanban/polling9.sm --N 200;
+#python3 runfile.py --model ctmc/kanban/polling9.sm --N 400;
+###
+#python3 runfile.py --model ctmc/kanban/polling12.sm --N 50;
+python3 runfile.py --model ctmc/kanban/polling12.sm --N 100;
+#python3 runfile.py --model ctmc/kanban/polling12.sm --N 200;
+#python3 runfile.py --model ctmc/kanban/polling12.sm --N 400;
+###
+#python3 runfile.py --model ctmc/kanban/polling15.sm --N 50;
+python3 runfile.py --model ctmc/kanban/polling15.sm --N 100;
+#python3 runfile.py --model ctmc/kanban/polling15.sm --N 200;
+#python3 runfile.py --model ctmc/kanban/polling15.sm --N 400;
