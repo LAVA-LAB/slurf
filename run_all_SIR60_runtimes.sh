613b1d0cc52c5ba49f9eaded5d87d00f19059eaf
Thom Badings <thom.badings@ru.nl>
2022-01-13 14:24:44 +0100

    run script for SIR60 runtime validation

--- run_all_SIR60_runtimes.sh
+++ run_all_SIR60_runtimes.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-python3 runfile.py --model ctmc/epidemic/sir60.sm --N 50 --Nvalidation 1000 --repeat 10;
-python3 runfile.py --model ctmc/epidemic/sir60.sm --N 100 --Nvalidation 1000 --repeat 10;
-python3 runfile.py --model ctmc/epidemic/sir60.sm --N 200 --Nvalidation 1000 --repeat 10;
-python3 runfile.py --model ctmc/epidemic/sir60.sm --N 400 --Nvalidation 1000 --repeat 10;
+python3 runfile.py --model ctmc/epidemic/sir60.sm --N [50,100,200,400,800] --prop_file properties25.xlsx --repeat 5; 
+python3 runfile.py --model ctmc/epidemic/sir60.sm --N [50,100,200,400,800] --prop_file properties50.xlsx --repeat 5; 
+python3 runfile.py --model ctmc/epidemic/sir60.sm --N [50,100,200,400,800] --prop_file properties100.xlsx --repeat 5; 
+python3 runfile.py --model ctmc/epidemic/sir60.sm --N [50,100,200,400,800] --prop_file properties200.xlsx --repeat 5; 
+python3 runfile.py --model ctmc/epidemic/sir60.sm --N [50,100,200,400,800] --prop_file properties400.xlsx --repeat 5; 
+python3 runfile.py --model ctmc/epidemic/sir60.sm --N [50,100,200,400,800] --prop_file properties800.xlsx --repeat 5; 
