8be01ed597c9ff63d3c957aef1476c5ae27b2598
Thom Badings <thom.badings@ru.nl>
2022-05-06 14:20:08 +0200

    changed nr validation samples in partial exps

--- experiments/table2_bounds_partial.sh
+++ experiments/table2_bounds_partial.sh
@@ -4,7 +4,7 @@ echo -e "++++++++ RUN KANBAN(3) BENCHMARK ++++++++\n";
 reps=3
 for (( i=0; i < $reps; ++i )); do
 	for N in 100 200; do
-		python3 runfile.py --model ctmc/kanban/kanban3.sm --N $N --rho 1.1 --no-bisim --Nvalidate 500 --seed $i --export_stats output/stats_bounds/kanban/kanban3_${N}_s${i}.json;
+		python3 runfile.py --model ctmc/kanban/kanban3.sm --N $N --rho 1.1 --no-bisim --Nvalidate 200 --seed $i --export_stats output/stats_bounds/kanban/kanban3_${N}_s${i}.json;
 	done
 done
 python generate_table.py --folder 'output/stats_bounds/kanban/' --outfile 'output/table2_kanban_partial.csv' --mode lower_bounds;
@@ -13,7 +13,7 @@ echo -e "\n++++++++ RUN KANBAN(3) BENCHMARK ++++++++\n";
 reps=3
 for (( i=0; i < $reps; ++i )); do
 	for N in 100 200; do
-		python3 runfile.py --model dft/rc/rc.1-1-hc_parametric.dft --param_file rc.1-1-hc_parameters.xlsx --N $N --rho 1.1 --Nvalidate 500 --seed $i --export_stats output/stats_bounds/rc/rc1-1_${N}_s$i.json;
+		python3 runfile.py --model dft/rc/rc.1-1-hc_parametric.dft --param_file rc.1-1-hc_parameters.xlsx --N $N --rho 1.1 --Nvalidate 200 --seed $i --export_stats output/stats_bounds/rc/rc1-1_${N}_s$i.json;
 	done
 done
 python generate_table.py --folder 'output/stats_bounds/rc/' --outfile 'output/table2_rc_partial.csv' --mode lower_bounds;
