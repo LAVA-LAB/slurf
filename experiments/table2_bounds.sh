ae94e10b55c6b78ea5e61898737a43eb7002e29e
Thom Badings <thom.badings@ru.nl>
2022-05-06 13:29:30 +0200

    bugfix in Table2 partial experiment

--- experiments/table2_bounds.sh
+++ experiments/table2_bounds.sh
@@ -5,6 +5,7 @@ reps=10
 for (( i=0; i < $reps; ++i )); do
 	for N in 100 200 400 800; do
 		python3 runfile.py --model ctmc/kanban/kanban3.sm --N $N --rho 1.1 --no-bisim --Nvalidate 1000 --seed $i --export_stats output/stats_bounds/kanban/kanban3_${N}_s${i}.json;
+	done
 done
 python generate_table.py --folder 'output/stats_bounds/kanban/' --outfile 'experiments/results/table2_kanban.csv' --mode lower_bounds;
 #
@@ -13,5 +14,6 @@ reps=10
 for (( i=0; i < $reps; ++i )); do
 	for N in 100 200 400 800; do
 		python3 runfile.py --model dft/rc/rc.1-1-hc_parametric.dft --param_file rc.1-1-hc_parameters.xlsx --N $N --rho 1.1 --Nvalidate 1000 --seed $i --export_stats output/stats_bounds/rc/rc1-1_${N}_s$i.json;
+	done
 done
 python generate_table.py --folder 'output/stats_bounds/rc/' --outfile 'experiments/results/table2_rc.csv' --mode lower_bounds;
--- experiments/table2_bounds_partial.sh
+++ experiments/table2_bounds_partial.sh
@@ -5,6 +5,7 @@ reps=3
 for (( i=0; i < $reps; ++i )); do
 	for N in 100 200; do
 		python3 runfile.py --model ctmc/kanban/kanban3.sm --N $N --rho 1.1 --no-bisim --Nvalidate 500 --seed $i --export_stats output/stats_bounds/kanban/kanban3_${N}_s${i}.json;
+	done
 done
 python generate_table.py --folder 'output/stats_bounds/kanban/' --outfile 'experiments/results/table2_kanban_partial.csv' --mode lower_bounds;
 #
@@ -13,5 +14,6 @@ reps=3
 for (( i=0; i < $reps; ++i )); do
 	for N in 100 200; do
 		python3 runfile.py --model dft/rc/rc.1-1-hc_parametric.dft --param_file rc.1-1-hc_parameters.xlsx --N $N --rho 1.1 --Nvalidate 500 --seed $i --export_stats output/stats_bounds/rc/rc1-1_${N}_s$i.json;
+	done
 done
 python generate_table.py --folder 'output/stats_bounds/rc/' --outfile 'experiments/results/table2_rc_partial.csv' --mode lower_bounds;
